2617a8c894485b403103e96e2758b3d0eb588e69
MrDronax <39879193+MrDronax@users.noreply.github.com>
2019-02-08 00:20:28 +0100

    Update Exo 8(Rock_Paper_Scissors).py

--- Exo 8(Rock_Paper_Scissors).py
+++ Exo 8(Rock_Paper_Scissors).py
@@ -6,15 +6,15 @@ while(1):
         if(player1_move==player2_move):
             print("It's a tie")
         else:
-			#list of tuples
-            list=[]
+            list=[] #list of tuples
             list.append((1,player1_move))
             list.append((2,player2_move))
             
-			#This will convert our list of Tuples into dictionaries where keys are item[0] of tuple 
-			#and values are item[1] of tuple
-			dicdoc=dict(list) 
-            if(dicdoc.get(1) in "PaperRock" and dicdoc.get(2) in "PaperRock"):
+	    #This will convert our list of Tuples into dictionaries where keys are item[0] of tuple 
+	    #and values are item[1] of tuple
+	    dicdoc=dict(list) 
+            
+	    if(dicdoc.get(1) in "PaperRock" and dicdoc.get(2) in "PaperRock"):
                 Winner_ID=[k for k,v in dicdoc.items() if(v=="Paper")]
             elif(dicdoc.get(1) in "RockScissors" and dicdoc.get(2) in "RockScissors"):
                 Winner_ID=[k for k,v in dicdoc.items() if(v=="Rock")]
