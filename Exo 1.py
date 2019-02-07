4aa21fe98340f5a55e8c913faca3452d1f5eea75
MrDronax <39879193+MrDronax@users.noreply.github.com>
2019-02-07 21:58:42 +0100

    Update Exo 1.py

--- Exo 1.py
+++ Exo 1.py
@@ -1,5 +1,10 @@
 name=input("Entrez votre nom: ")
 age=int(input("Entrez votre age: "))
-number=int(input("Entrez nombre de repetition: "))
+repeat=int(input("Entrez nombre de repetition: "))
+
 s=name+" will be 100 years old in: "+str(2018+(100-age))+"\n"
-print(number * s)
+print(repeat * s)
+
+#Method 2:
+#for x in range(repeat):
+#   print("you will be 100 years old in the year "+str(tmp))
