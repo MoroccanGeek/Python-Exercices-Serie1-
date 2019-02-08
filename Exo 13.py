75043c92d65089f7df8890ae78eff4edc211739e
MrDronax <39879193+MrDronax@users.noreply.github.com>
2019-02-08 12:32:58 +0100

    Update Exo 13.py

--- Exo 13.py
+++ Exo 13.py
@@ -11,4 +11,25 @@ def Fib(maxi):
         S1=S2
         S2=Sum
     return seq
-print(Fib(10))
+
+def Fib2(n):
+    f1=1
+    f2=1
+    w=[]
+    if(n>0 and n<=2):
+        w=[1 for x in range(n)]
+    
+    else:
+        w=[1,1]
+        i=0
+        j=1
+        for x in range(2,n):
+            w.append(w[i]+w[j])
+            i=i+1
+            j=j+1
+    
+    return w
+
+sequence_number=int(input("Enter how many Fibonnaci numbers to generate"))
+print(Fib2(sequence_number))
+  
